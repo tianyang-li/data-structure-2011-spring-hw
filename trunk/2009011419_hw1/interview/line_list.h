7aef93488228d6228a8e4da1780c34af984ccb1b
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-03-05 13:12:18 +0000

    DJAjojorge

--- trunk/2009011419_hw1/interview/line_list.h
+++ trunk/2009011419_hw1/interview/line_list.h
@@ -5,4 +5,52 @@
 #ifndef LINE_LIST_H
 #define LINE_LIST_H
 
+#include <iostream>
+
+template <class T>
+class LineList {
+public:
+	struct LineListNode {
+		LineListNode();
+		LineListNode *next_, *prev_;
+		T data_;
+	};
+
+	LineList();
+	~LineList();
+
+	std::size_t GetLen() const {
+		return len_;
+	}
+	bool Empty() const {
+		// return true if empty
+		return (head_ == tail_);
+	}
+
+protected:
+	LineListNode *head_, *tail_;
+	std::size_t len_;  // legnth of list
+
+	void Init();
+};
+
+template <class T>
+LineList<T>::LineListNode::LineListNode() : next_(NULL), prev_(NULL) {
+}
+
+template <class T>
+LineList<T>::LineList() : len_(0) {
+	Init();
+}
+
+template <class T>
+void LineList<T>::Init() {
+	head_ = tail_ = new (std::nothrow) LineList<T>::LineListNode;
+
+	if (head_ == NULL) {
+		std::cerr << "head_ = tail_ = new (std::nothrow) LineList<T>::LineListNode";
+		std::cerr << std::endl << "memory allocation error" << std::endl;
+	}
+}
+
 #endif  // LINE_LIST_H
