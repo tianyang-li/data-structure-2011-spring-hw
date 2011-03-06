534871ff5ce96f759a0d782aee7c7d37675fdfc9
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-03-06 15:30:32 +0000

    modify this into a circular list

--- 2009011419_hw1/interview/main.cc
+++ 2009011419_hw1/interview/main.cc
@@ -2,7 +2,27 @@
 
 #include "line_list.h"
 
+class Interview {
+public:
+	Interview();
+	~Interview();
+private:
+	void Init();
+
+	LineList<int> people_;
+
+	int N_;  // # of interviewees
+	int m_;  // separation distance
+};
+
+Interview::Interview() {
+	this->Init();
+}
+
+void Interview::Init() {
+	cin >> this->N_ >> this->m_;
+}
+
 int main(int argc, char **argv) {
-	LineList<int> test1;
 	return 0;
 }
