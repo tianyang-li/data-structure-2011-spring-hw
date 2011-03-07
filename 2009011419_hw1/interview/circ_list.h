2139e6495e3e0aed3d3482c4ddd86794fe3c972c
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-03-07 15:09:42 +0000

    fwefpkewpkf

--- 2009011419_hw1/interview/circ_list.h
+++ 2009011419_hw1/interview/circ_list.h
@@ -7,6 +7,7 @@
 
 #include <cstddef>
 #include <new>
+#include <iostream>
 
 #include "list_node.h"
 
@@ -21,7 +22,8 @@ public:
 	}
 
 	// insert new_T at position pos in the list
-	bool Insert(T const &new_T, std::size_t pos = 0);
+	// return a pointer to the new node
+	ListNode<T> *Insert(T const &new_T, std::size_t pos = 0);
 
 	// delete data at position pos in the list
 	// return value of data stored at (pos % this->len_) before deletion
@@ -33,6 +35,14 @@ public:
 		return (this->list_ptr_ == NULL);
 	}
 
+	// return a ptr to the new node
+	// cur_list.list_ptr_ NOT modified
+	static ListNode<T> *InsertAfter(CircList<T> &cur_list, ListNode<T> *cur_node, T const &new_T);
+
+	// return a ptr to the new node
+	// cur_list.list_ptr_ NOT modified
+	static ListNode<T> *InsertBefore(CircList<T> &cur_list, ListNode<T> *cur_node, T const &new_T);
+
 protected:
 	ListNode<T> *list_ptr_;  // point to the actual list storing data
 
@@ -40,16 +50,69 @@ protected:
 };
 
 template <class T>
-bool CircList<T>::Insert(T const &new_T, std::size_t pos) {
+ListNode<T> *CircList<T>::InsertAfter(CircList<T> &cur_list, ListNode<T> *cur_node, T const &new_T) {
 	ListNode<T> *new_node = new (std::nothrow) ListNode<T>(new_T);
+
+	if (new_node == NULL) {
+		std::cerr << "ListNode<T> *new_node = new (std::nothrow) ListNode<T>(new_T)";
+		std::cerr << std::endl << "memory allocation failed" << std::endl;
+		return NULL;
+	}
+
+	++cur_list.len_;
+
+	new_node->prev_ = cur_node;
+	new_node->next_ = cur_node->next_;
+
+	cur_node->next_->prev_ = new_node;
+	cur_node->next_ = new_node;
+
+	return new_node;
+}
+
+template <class T>
+ListNode<T> *CircList<T>::InsertBefore(CircList<T> &cur_list, ListNode<T> *cur_node, T const &new_T) {
+	ListNode<T> *new_node = new (std::nothrow) ListNode<T>(new_T);
+
+	if (new_node == NULL) {
+		std::cerr << "ListNode<T> *new_node = new (std::nothrow) ListNode<T>(new_T)";
+		std::cerr << std::endl << "memory allocation failed" << std::endl;
+		return NULL;
+	}
+
+	++cur_list.len_;
+
+	new_node->next_ = cur_node;
+	new_node->prev_ = cur_node->prev_;
+
+	cur_node->prev_->next_ = new_node;
+	cur_node->prev_ = new_node;
+
+	return new_node;
+}
+
+template <class T>
+ListNode<T> * CircList<T>::Insert(T const &new_T, std::size_t pos) {
+	ListNode<T> *new_node = new (std::nothrow) ListNode<T>(new_T);
+
+	if (new_node == NULL) {
+		std::cerr << "ListNode<T> *new_node = new (std::nothrow) ListNode<T>(new_T)";
+		std::cerr << std::endl << "memory allocation failed" << std::endl;
+		return NULL;
+	}
+
+	++this->len_;
+
+	return new_node;
 }
 
 template <class T>
 T CircList<T>::Delete(std::size_t pos) {
 	T del_data;
+	std::size_t cur_pos = pos % (this->len_);
 
 	if (!this->Empty()) {
-
+		--this->len_;
 	}
 
 	return del_data;
