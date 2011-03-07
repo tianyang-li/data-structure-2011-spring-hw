d3b935fca3b92188b97d15704ed9d49c6fadf83f
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-03-07 00:35:32 +0000

    write a circular list

--- 2009011419_hw1/interview/main.cc
+++ 2009011419_hw1/interview/main.cc
@@ -4,23 +4,40 @@
 
 class Interview {
 public:
+	typedef int ID;
 	Interview();
 	~Interview();
 private:
-	void Init();
-
-	LineList<int> people_;
+	LineList<Interview::ID> people_;
+	ListNode<Interview::ID> *insert_after_;
 
 	int N_;  // # of interviewees
 	int m_;  // separation distance
+
+	void Init();
+
+	// move cur_person_ past m people so new ID can be inserted after cur_person_
+	void MovePastM();
 };
 
-Interview::Interview() {
+Interview::Interview() : insert_after_(NULL) {
 	this->Init();
 }
 
+void Interview::MovePastM() {
+}
+
 void Interview::Init() {
-	cin >> this->N_ >> this->m_;
+	this->insert_after_ = this->people_.GetSentryHead();
+
+	std::cin >> this->N_ >> this->m_;
+
+	Interview::ID temp_ID;
+	for (int i = 0; i != this->N_; ++i) {
+		std::cin >> temp_ID;
+
+		this->MovePastM();
+	}
 }
 
 int main(int argc, char **argv) {
