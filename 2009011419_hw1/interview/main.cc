54082855c05df553d8f3c288c2617f2493196a87
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-03-10 23:42:43 +0000

    bug with static?

--- 2009011419_hw1/interview/main.cc
+++ 2009011419_hw1/interview/main.cc
@@ -1,17 +1,24 @@
 #include <iostream>
 
 #include "circ_list.h"
+#include "list_node.h"
 
 class Interview {
 public:
-	typedef unsigned int ID;
 	Interview();
 	~Interview();
 
 private:
+	CircList<int> people_;
+
+	int N_;  // # of interviewee
+	int m_;  // # to skip each time
+
+	ListNode<int> *cur_node_;  // current node to insert after;
+
 	void Init();
 
-	CircList<Interview::ID> people_;
+	void MoveByM();  // move cur_node_ through the list as stated
 };
 
 Interview::Interview() {
@@ -19,11 +26,24 @@ Interview::Interview() {
 }
 
 Interview::~Interview() {
+}
 
+void Interview::MoveByM() {
 }
 
 void Interview::Init() {
+	std::cin >> this->N_ >> this->m_;
 
+	int temp_ID;
+
+	std::cin >> temp_ID;
+	this->cur_node_ = this->people_.InsertAfter(temp_ID, 0);
+
+	for (int i = 1; i != this->N_; ++i) {
+		std::cin >> temp_ID;
+		this->MoveByM();
+		CircList<int>::InsertAfter(*this, this->cur_node_, temp_ID);
+	}
 }
 
 int main(int argc, char **argv) {
