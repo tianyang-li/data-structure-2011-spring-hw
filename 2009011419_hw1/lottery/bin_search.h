4d54a8901c0edbdffc8480f75f3c2f36845f029b
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-03-12 15:10:41 +0000

    joifrewfjo

---
+++ 2009011419_hw1/lottery/bin_search.h
@@ -0,0 +1,16 @@
+/*
+ * template for
+ * binary search
+ */
+
+#ifndef BIN_SEARCH_H
+#define BIN_SEARCH_H
+
+template <class T>
+bool BinSearch(int low, int high, T *array, T const &query) {
+	// search for query in [low, high] in array
+	// true if found
+	return false;
+}
+
+#endif  // BIN_SEARCH_H
