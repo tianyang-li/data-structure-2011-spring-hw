f3d0b774b698dfe10591c22b46474ad7c32c02fa
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-03-12 15:09:22 +0000

    fiwofj

---
+++ 2009011419_hw1/lottery/quick_sort.h
@@ -0,0 +1,14 @@
+/*
+ * template for quick sort
+ */
+
+#ifndef QUICK_SORT_H
+#define QUICK_SORT_H
+
+template <class T>
+void QuickSort(T *array, int low, int high) {
+	// sort array from lowest to highest in range [low, high]
+}
+
+#endif  // QUICK_SORT_H
+
