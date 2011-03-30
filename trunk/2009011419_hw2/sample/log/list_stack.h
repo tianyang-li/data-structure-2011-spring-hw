954a03a5ea8c7ace19b0bfedec139f10ef4b35ea
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-03-30 12:42:33 +0000

    fjwoire

--- trunk/2009011419_hw2/sample/log/list_stack.h
+++ trunk/2009011419_hw2/sample/log/list_stack.h
@@ -25,32 +25,86 @@
 #ifndef LIST_STACK_H
 #define LIST_STACK_H
 
+#include <cstddef>
+#include <new>
+
 #include "list_node.h"
 
 template <class T>
-class ListStack {
+class ListStack {  // similar to STL stack
 public:
 	ListStack();
 	~ListStack();
 
+	inline bool Empty() const;
+	inline std::size_t Size() const;
+	inline T Top() const;  // get top of stack, undefined when this->top_ == this->sentry_
+
+	inline void Pop();
+	inline void Push(T const &new_T);
 protected:
 	ListNode<T> *top_;  // top of stack
+	ListNode<T> *sentry_;
+
+	std::size_t size_;
+
+	inline void Init();
 };
 
 template <class T>
-ListStack<T>::ListStack() : top_(NULL) {
+void ListStack<T>::Init() {
+	this->sentry_ = ListNode<T>::CreateNewNode();
+	this->top_ = this->sentry_;
+}
+
+template <class T>
+void ListStack<T>::Push(T const &new_T) {
+	++this->size_;
+	ListNode<T> *temp = ListNode<T>::CreateNewNode(new_T);
+	temp->prev_ = this->top_;
+	this->top_ = temp;
+}
+
+template <class T>
+void ListStack<T>::Pop() {
+	if (this->sentry_ == this->top_) {
+		return;
+	}
+
+	--this->size_;
+	ListNode<T> * temp = this->top_;
+	this->top_ = temp->prev_;
+	delete temp;
+}
+
+template <class T>
+T ListStack<T>::Top() const {
+	return this->top_->data_;
+}
+
+template <class T>
+std::size_t ListStack<T>::Size() const {
+	return this->size_;
+}
+
+template<class T>
+bool ListStack<T>::Empty() const {
+	return (this->sentry_ == this->top_);
+}
+
+template <class T>
+ListStack<T>::ListStack() : top_(NULL), sentry_(NULL), size_(0) {
+	this->Init();
 }
 
 template <class T>
 ListStack<T>::~ListStack() {
-	if (this->top_ != NULL) {
-		ListNode<T> *temp1 = this->top_, *temp2 = this->top_->next_;
-		while (temp1 != NULL) {
-			delete temp1;
-			temp1 = temp2;
-			if (temp1 != NULL) {
-				temp2 = temp1->next_;
-			}
+	ListNode<T> *temp1 = this->top_, *temp2 = this->top_->prev_;
+	while (temp1 != NULL) {
+		delete temp1;
+		temp1 = temp2;
+		if (temp1 != NULL) {
+			temp2 = temp1->prev_;
 		}
 	}
 }
