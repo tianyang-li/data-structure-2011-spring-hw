0b39b875ab766bcdd0ab740dbcb5c7c316525b0b
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-04-11 23:30:00 +0000

    fjeio

--- trunk/2009011419_hw2/sample/hospital/list_node.h
+++ trunk/2009011419_hw2/sample/hospital/list_node.h
@@ -1,4 +1,24 @@
 /*
+ *  Copyright (C) 2011 Tianyang Li
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author Tianyang Li
+ * E-Mail tmy1018 gmail com
+ */
+
+/*
  * generic list node
  */
 
@@ -14,6 +34,7 @@ struct ListNode {
 	ListNode(T const &new_T);
 
 	static ListNode<T> *CreateNewNode(T const &new_T);
+	static ListNode<T> *CreateNewNode();
 
 	ListNode<T> *next_;
 	ListNode<T> *prev_;
@@ -24,7 +45,7 @@ struct ListNode {
 template <class T>
 ListNode<T> *ListNode<T>::CreateNewNode(T const &new_T) {
 	ListNode<T> *new_node;
-	new_node = new (std::nothrow) ListNode<T>;
+	new_node = new (std::nothrow) ListNode<T>(new_T);
 
 	if (new_node == NULL) {
 		std::cerr << "new_node = new (std::nothrow) ListNode<T>";
@@ -32,7 +53,19 @@ ListNode<T> *ListNode<T>::CreateNewNode(T const &new_T) {
 		return NULL;
 	}
 
-	new_node->data_ = new_T;
+	return new_node;
+}
+
+template <class T>
+ListNode<T> *ListNode<T>::CreateNewNode() {
+	ListNode<T> *new_node;
+	new_node = new (std::nothrow) ListNode<T>();
+
+	if (NULL == new_node) {
+		std::cerr << "new_node = new (std::nothrow) ListNode<T>";
+		std::cerr << std::endl << "memory allocation error" << std::endl;
+		return NULL;
+	}
 
 	return new_node;
 }
