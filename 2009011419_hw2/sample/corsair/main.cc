ae04126d46474ea70d87a3fab9fccc014f6a94cd
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-04-13 13:41:45 +0000

    fjijewoi

--- 2009011419_hw2/sample/corsair/main.cc
+++ 2009011419_hw2/sample/corsair/main.cc
@@ -32,12 +32,15 @@ public:
 
 private:
 	int n_;  // # of coins
+	int n_ind_;  // = n_ - 1
 	int val_[Corsair::kMaxCoin];  // value of coins
 	int owner_[Corsair::kMaxCoin];  // owner_[i] is the owner of the i^th coin
 	int tot_;  // total val of coins
 	int asset_[Corsair::kMaxCoin];
-//	static int const kNoOwner = -1;
 	int cur_max_owner_;  // because owners are searched from 0 up, this says
+	int cur_coin_;  // current coin searching for owner, sort of like a stack ptr
+	int min_coin_[Corsair::kMaxCoin];  // min_coin_[i] is lowest index in the indices of coins owned by the i^th person
+	int i[Corsair::kMaxCoin];  // indices used in search
 
 	inline void SortCoin();
 	inline bool DivPossible(int people);
@@ -59,18 +62,17 @@ inline bool Corsair::DivPossible(int people) {
 	}
 	int subset = this->tot_ / people;  // what each person should get
 	this->owner_[0] = 0;  // 0^th person has the 0^th coin
-/*	for (int i = 1; i != this->n_; ++i) {
-		this->owner_[i] = Corsair::kNoOwner;
-	}*/
 	this->asset_[0] = this->val_[0];
 	for (int i = 1; i != people; ++i) {
 		this->asset_[i] = 0;
 	}
 	this->cur_max_owner_ = 0;
-	while (this->NotReached(subset, people)) {
-		return false; //TODO
+	this->cur_coin_ = 1;
+	this->min_coin_[0] = 0;
+	while (/*this->NotReached(subset, people) && (this->cur_coin_ != this->n_)*/true) {
+		return true;
 	}
-	return true;
+	return false;
 }
 
 inline int Corsair::Most() {
@@ -89,7 +91,7 @@ Corsair::Corsair() : tot_(0) {
 
 inline void Corsair::SortCoin() {  // bubble sort
 	int temp_swap;
-	for (int i = this->n_ - 1; i != -1; --i) {
+	for (int i = this->n_ind_; i != -1; --i) {
 		for (int j = 0; j != i; ++j) {
 			if (this->val_[j] < this->val_[j + 1]) {
 				temp_swap = this->val_[j];
@@ -106,6 +108,7 @@ inline void Corsair::Init() {
 		std::cin >> this->val_[i];
 		this->tot_ += this->val_[i];
 	}
+	this->n_ind_ = this->n_ - 1;
 	this->SortCoin();
 }
 
