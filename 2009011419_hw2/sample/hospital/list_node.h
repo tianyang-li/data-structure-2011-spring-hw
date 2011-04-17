be364ebe24a33fe0fd64079ac705661b8228e47f
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-04-17 08:10:44 +0000

    jrvgejo

--- 2009011419_hw2/sample/hospital/list_node.h
+++ 2009011419_hw2/sample/hospital/list_node.h
@@ -33,8 +33,8 @@ struct ListNode {
 	ListNode();
 	ListNode(T const &new_T);
 
-	static ListNode<T> *CreateNewNode(T const &new_T);
-	static ListNode<T> *CreateNewNode();
+	inline static ListNode<T> *CreateNewNode(T const &new_T);
+	inline static ListNode<T> *CreateNewNode();
 
 	ListNode<T> *next_;
 	ListNode<T> *prev_;
@@ -43,29 +43,17 @@ struct ListNode {
 };
 
 template <class T>
-ListNode<T> *ListNode<T>::CreateNewNode(T const &new_T) {
+inline ListNode<T> *ListNode<T>::CreateNewNode(T const &new_T) {
 	ListNode<T> *new_node;
 	new_node = new (std::nothrow) ListNode<T>(new_T);
-
-	if (new_node == NULL) {
-		std::cerr << "new_node = new (std::nothrow) ListNode<T>";
-		std::cerr << std::endl << "memory allocation error" << std::endl;
-		return NULL;
-	}
 
 	return new_node;
 }
 
 template <class T>
-ListNode<T> *ListNode<T>::CreateNewNode() {
+inline ListNode<T> *ListNode<T>::CreateNewNode() {
 	ListNode<T> *new_node;
 	new_node = new (std::nothrow) ListNode<T>();
-
-	if (NULL == new_node) {
-		std::cerr << "new_node = new (std::nothrow) ListNode<T>";
-		std::cerr << std::endl << "memory allocation error" << std::endl;
-		return NULL;
-	}
 
 	return new_node;
 }
