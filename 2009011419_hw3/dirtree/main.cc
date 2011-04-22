c7f6454033db5fb29fc42479007329532f935927
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-04-22 14:42:25 +0000

    foiwjeoi

--- 2009011419_hw3/dirtree/main.cc
+++ 2009011419_hw3/dirtree/main.cc
@@ -1,5 +1,38 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+class DirTree {
+private:
+	static int const kMaxLen = 256;  // max len of path names
+
+public:
+	typedef char Path[kMaxLen];
+
+	inline DirTree() {
+	}
+
+	inline ~DirTree() {
+	}
+
+	inline void Init();
+
+private:
+};
+
+inline void DirTree::Init() {
+	int n;
+	cin >> n;
+	Path tmp_path;
+	for (int i = 0; i != n; ++i) {
+		scanf("%s", tmp_path);
+	}
+}
 
 int main(int argc, char **argv) {
+	DirTree dir_tree;
+	dir_tree.Init();
 	return 0;
 }
