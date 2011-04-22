3972b7d5ef5a76e01ed172acffdc3a06fa5846d3
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-04-22 03:53:27 +0000

    jrgeioj

---
+++ 2009011419_hw3/battle/main.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class Battle {
+public:
+	Battle() {
+		Init();
+	}
+
+	~Battle() {
+	}
+
+private:
+	void Init() {
+	}
+};
+
+int main(int argc, char **argv) {
+	Battle battle;
+	return 0;
+}
