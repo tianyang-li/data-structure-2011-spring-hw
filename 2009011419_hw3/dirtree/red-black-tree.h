8fad9345f1e7ab58d074d9f0b6e2fa26f46decef
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-04-24 15:31:18 +0000

    gfjweo

---
+++ 2009011419_hw3/dirtree/red-black-tree.h
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (C) 2011 Tianyang Li
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author Tianyang Li
+ * E-Mail tmy1018 gmail com
+ */
+
+#ifndef RED_BLACK_TREE_H
+#define RED_BLACK_TREE_H
+
+#include <new>
+#include <iostream>
+#include <cstddef>
+
+template <class T>
+class RBTree {
+public:
+	class Node {
+	public:
+		inline Node() : par(NULL), rc(NULL), lc(NULL) {
+		}
+
+		inline Node(T const &new_data) : par(NULL), rc(NULL), lc(NULL), data(new_data) {
+		}
+
+		Node *par;  // parent
+		Node *rc, *lc;  // right-child, left-child
+		T data;
+	private:
+	};
+	typedef Node *NodePtr;
+
+	NodePtr root;
+
+	inline RBTree() : root(NULL) {
+	}
+
+private:
+};
+
+#endif
+
