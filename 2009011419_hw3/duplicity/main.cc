e7ca326bae262aa833df19627bfaf3d200b0b3cc
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-05-04 06:30:05 +0000

    class Word in class Duplicity

--- 2009011419_hw3/duplicity/main.cc
+++ 2009011419_hw3/duplicity/main.cc
@@ -1,15 +1,37 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <cstddef>
 
 #include "hash1.h"
 
 using namespace std;
 
 class Duplicity {
+private:
+	static size_t const kMaxLen = 40;
+
+	class Word {
+	public:
+		char word[kMaxLen];
+
+		inline bool operator==(Word const &cur) const {
+			size_t i = 0;
+			while ((cur.word[i] != '\0') || (word[i] != '\0')) {
+				if (cur.word[i] != word[i]) {
+					return false;
+				}
+				++i;
+			}
+			return true;
+		}
+	};
+
 public:
 	inline Duplicity();
 	inline ~Duplicity();
+
+	inline void Proc();
 
 private:
 	inline void Init();
@@ -25,8 +47,12 @@ inline Duplicity::~Duplicity() {
 inline void Duplicity::Init() {
 }
 
+inline void Duplicity::Proc() {
+	return;
+}
+
 int main(int argc, char **argv) {
 	Duplicity dup;
-
+	dup.Proc();
 	return 0;
 }
