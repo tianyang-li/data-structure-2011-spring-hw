8c51f713d3e346e3602d7bbf19c6353ebc39e95a
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-05-05 05:54:04 +0000

    fjewjoi

---
+++ 2009011419_hw3/duplicity/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import sys
+
+str = 'a\nb\nc\nd\ne\nf\ng\nh\ni\nj\nk\nl\nm\nn\no\np\nq\nr\ns\nt\nu\nv\nw\nx\ny\nz'
+
+
+i = 1
+while i != int(sys.argv[1]):
+	print str
+	i = i + 1
+
+
+
