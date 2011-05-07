307bb90cf0a833b6b54e26523dd5f9baebc071b5
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-05-07 05:03:47 +0000

    work on Query and BuildTree

--- 2009011419_hw3/temperature/main.cc
+++ 2009011419_hw3/temperature/main.cc
@@ -8,21 +8,55 @@
 using namespace std;
 
 class Temp {
+private:
+	struct Point {
+		int x, y;
+	};
+
 public:
 	inline Temp();
 	inline ~Temp();
+	inline void Init();
+	inline void Proc();
 
 private:
-	RangeTree<int> stat;  // stationss
+	RangeTree<int, float> stat;  // stationss
 	size_t m;
+
+	inline float Query(Point const &LL, Point const &UR) const;
 };
 
-inline Temp::Temp() {
+inline float Temp::Query(Point const &LL, Point const &UR) const {
+}
+
+inline Temp::~Temp() {
+}
+
+inline void Temp::Init() {
 	size_t n;
 	scanf("%d %d", &n, &m);
 	stat.SetSize(n);
+	for (size_t i = 0; i != n; ++i) {
+		scanf("%d %d %f", &(stat.points[i].x), &(stat.points[i].y), &(stat.points[i].data));
+	}
+	stat.BuildTree();
+}
+
+inline Temp::Temp() {
+}
+
+inline void Temp::Proc() {
+	Point LL;  // lower left corner
+	Point UR;  // upper right corner
+	for (size_t i = 0; i != m; ++i) {
+		scanf("%d %d %d %d", &(LL.x), &(LL.y), &(UR.x), &(UR.y));  // inclusive (for stations)
+		printf("%.2f", Query(LL, UR));
+	}
 }
 
 int main(int argc,char **argv) {
+	Temp temp;
+	temp.Init();
+	temp.Proc();
 	return 0;
 }
