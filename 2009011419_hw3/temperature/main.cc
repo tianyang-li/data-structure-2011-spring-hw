84e97561bad321caad44013e782a26d150f7a84d
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-05-11 07:42:37 +0000

    modified a bit, do query

--- 2009011419_hw3/temperature/main.cc
+++ 2009011419_hw3/temperature/main.cc
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cstddef>
+#include <stdint.h>
 
 #include "range-tree.h"
 
@@ -11,7 +12,7 @@ class Temp {
 private:
 	struct Info {
 		size_t n;  // # of stations
-		float temp; // temperature
+		int64_t temp; // temperature
 
 		inline Info() : n(0), temp(0) {
 		}
@@ -28,16 +29,16 @@ private:
 	RangeTree<int, Info> stat;  // stationss
 	size_t m;
 
-	inline float Query(Point const &LL, Point const &UR) const;
+	inline int Query(Point const &LL, Point const &UR) const;
 	inline void PreProc(RangeTree<int, Info>::XNodePtr const root);
 	inline void ProcY(RangeTree<int, Info>::YNodePtr const root);
 	inline RangeTree<int, Info>::XNodePtr XFindSplit(int const x1, int const x2, RangeTree<int, Info>::XNodePtr const root) const;
-	inline float Average(int const x1, int const x2, int const y1, int const y2, RangeTree<int, Info>::XNodePtr split) const;
+	inline int Average(int const x1, int const x2, int const y1, int const y2, RangeTree<int, Info>::XNodePtr split) const;
 };
 
-inline float Temp::Average(int const x1, int const x2, int const y1, int const y2, RangeTree<int, Info>::XNodePtr split) const {
+inline int Temp::Average(int const x1, int const x2, int const y1, int const y2, RangeTree<int, Info>::XNodePtr split) const {
 	Info info;
-	return info.temp;
+	return int(info.temp/int64_t(info.n));
 }
 
 inline RangeTree<int, Temp::Info>::XNodePtr Temp::XFindSplit(int const x1, int const x2, RangeTree<int, Info>::XNodePtr const root) const {
@@ -61,10 +62,8 @@ inline void Temp::ProcY(RangeTree<int, Info>::YNodePtr const root) {
 	}
 	if (root->rc) {
 		ProcY(root->rc);
-		size_t n = root->point->data.n + root->rc->point->data.n;
-		root->point->data.temp = (float(root->point->data.n) * root->point->data.temp + float(root->rc->point->data.n) * root->rc->point->data.temp)
-				/ (float(n));
-		root->point->data.n = n;
+		root->point->data.n = root->point->data.n + root->rc->point->data.n;
+		root->point->data.temp = root->point->data.temp + root->rc->point->data.temp;
 	}
 }
 
@@ -78,7 +77,7 @@ inline void Temp::PreProc(RangeTree<int, Info>::XNodePtr const root) {
 	}
 }
 
-inline float Temp::Query(Point const &LL, Point const &UR) const {
+inline int Temp::Query(Point const &LL, Point const &UR) const {
 	// query range (x1, x2) X (y1, y2) is now
 	// ((x1|-inf),(x2|+inf)) X ((y1|-inf), ((y2|+inf)))
 	RangeTree<int, Info>::XNode *node = XFindSplit(LL.x, UR.x, stat.root);
@@ -96,7 +95,7 @@ inline void Temp::Init() {
 	scanf("%d %d", &n, &m);
 	stat.SetSize(n);
 	for (size_t i = 0; i != n; ++i) {
-		scanf("%d %d %f", &(stat.points[i].coord.x), &(stat.points[i].coord.y), &(stat.points[i].data.temp));
+		scanf("%d %d %lld", &(stat.points[i].coord.x), &(stat.points[i].coord.y), &(stat.points[i].data.temp));
 		stat.points[i].data.n = 1;
 	}
 	stat.BuildTree();
@@ -111,7 +110,7 @@ inline void Temp::Proc() {
 	Point UR;  // upper right corner
 	for (size_t i = 0; i != m; ++i) {
 		scanf("%d %d %d %d", &(LL.x), &(LL.y), &(UR.x), &(UR.y));  // inclusive (for stations)
-		printf("%.2f\n", Query(LL, UR));
+		printf("%d\n", Query(LL, UR));
 	}
 }
 
