be7077f4070071e932b87840df28b8899f031c8a
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-05-20 10:28:06 +0000

    do slides

--- 2009011419_hw4/slides/main.cc
+++ 2009011419_hw4/slides/main.cc
@@ -4,6 +4,39 @@
 
 using namespace std;
 
+class Slides {
+public:
+	struct Tuple {
+		float x, y;
+	};
+
+	typedef Tuple Label;
+
+	struct Piece {
+		Tuple ll, ur;  // lower left, upper right
+	};
+
+	inline Slides();
+	inline ~Slides();
+
+private:
+	int n;
+	Label *lab;
+	Piece *sld;
+};
+
+inline Slides::Slides() {
+	cin >> n;
+	lab = new (nothrow) Label[n];
+	sld = new (nothrow) Piece[n];
+}
+
+inline Slides::~Slides() {
+	delete [] lab;
+	delete [] sld;
+}
+
 int main() {
+	Slides slides;
 	return 0;
 }
