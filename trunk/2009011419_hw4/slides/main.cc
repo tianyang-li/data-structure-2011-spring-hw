3b1108458a6563a13951af0a2a658d2504a92566
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-05-23 07:09:42 +0000

    slides!!

--- trunk/2009011419_hw4/slides/main.cc
+++ trunk/2009011419_hw4/slides/main.cc
@@ -6,14 +6,64 @@ using namespace std;
 
 class Slides {
 public:
+	struct Node {
+		Node *prev, *next;
+		int ind;  // index of mate
+		Node *mate;
+
+		inline Node() : prev(NULL), next(NULL), mate(NULL) {
+		}
+	};
+
 	struct Tuple {
 		float x, y;
 	};
 
-	typedef Tuple Label;
+	struct Label {
+		Tuple coord;
+		int deg;  // "degree" of label -- number of pieces it lies on
+		Node *list;
+
+		inline Label() : deg(0) {
+			list = new (nothrow) Node;
+		}
+
+		inline ~Label() {
+			Node *tmp1 = list, *tmp2 = list->next;
+			while (tmp1) {
+				delete tmp1;
+				tmp1 = tmp2;
+				if (tmp1) {
+					tmp2 = tmp1->next;
+				}
+			}
+		}
+	};
 
 	struct Piece {
 		Tuple ll, ur;  // lower left, upper right
+		int ind;  // index of this piece
+		int page;  // page number on this piece, not determined until later
+		Node *list;
+
+		inline Piece() {
+			list = new (nothrow) Node;
+		}
+
+		inline ~Piece() {
+			Node *tmp1 = list, *tmp2 = list->next;
+			while (tmp1) {
+				delete tmp1;
+				tmp1 = tmp2;
+				if (tmp1) {
+					tmp2 = tmp1->next;
+				}
+			}
+		}
+
+		bool inline On(Tuple const &pt) {  // true if pt is on this piece
+			return (pt.x >= ll.x && pt.x <= ur.x && pt.y >= ll.y && pt.y <= ur.y);
+		}
 	};
 
 	inline Slides();
@@ -21,19 +71,31 @@ public:
 
 private:
 	int n;
-	Label *lab;
-	Piece *sld;
+	Label *lab;  // labels on slides
+	Piece *pc;  // pieces of slides
 };
 
 inline Slides::Slides() {
 	cin >> n;
 	lab = new (nothrow) Label[n];
-	sld = new (nothrow) Piece[n];
+	pc = new (nothrow) Piece[n];
+	for (int i = 0; i != n; ++i) {
+		scanf("%f %f %f %f", &pc[i].ll.x, &pc[i].ll.y, &pc[i].ur.x, &pc[i].ur.y);
+		pc[i].ind = i;
+	}
+	for (int i = 0; i != n; ++i) {
+		scanf("%f %f", &lab[i].coord.x, &lab[i].coord.y);
+		for (int j = 0; j != n; ++j) {
+			if (pc[j].On(lab[i].coord)) {
+				++lab[i].deg;
+			}
+		}
+	}
 }
 
 inline Slides::~Slides() {
 	delete [] lab;
-	delete [] sld;
+	delete [] pc;
 }
 
 int main() {
