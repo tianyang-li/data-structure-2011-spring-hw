55c878c9a5215a981cade0a2b127231b9e953ec0
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-05-25 10:11:33 +0000

    sched

--- trunk/2009011419_hw4/scheduler/priority-queue.h
+++ trunk/2009011419_hw4/scheduler/priority-queue.h
@@ -4,16 +4,33 @@
 #include <new>
 #include <cstddef>
 
+// min heap
 template <class T>
 class PrQue {
 public:
-	inline PrQue() : size(0) {
+	std::size_t size;
+	T *data;
+
+	inline PrQue() {
 	}
 
 	inline ~PrQue() {
+		delete [] data;
 	}
-private:
-	std::size_t size;
+
+	inline void SetSize(std::size_t const new_size) {
+		size = new_size;
+		data = new (std::nothrow) T[new_size];  // XXX: error detection
+	}
+
+	void Insert(T const &new_data) {
+		++size;
+	}
+
+	std::size_t Pop() { // remove top
+		--size;
+		return (size + 1);
+	}
 };
 
 #endif
