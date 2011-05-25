f13d7304871fc1d369bbb8709f759fd42177b1ba
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-05-25 13:29:05 +0000

    sched!

--- 2009011419_hw4/scheduler/priority-queue.h
+++ 2009011419_hw4/scheduler/priority-queue.h
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (C) 2011 Tianyang Li
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author Tianyang Li
+ * E-Mail tmy1018 gmail com
+ */
+
 #ifndef PRIOR_QUEUE_H
 #define PRIOR_QUEUE_H
 
@@ -23,11 +43,11 @@ public:
 		data = new (std::nothrow) T[new_size];  // XXX: error detection
 	}
 
-	void Insert(T const &new_data) {
+	inline void Insert(T const &new_data) {
 		++size;
 	}
 
-	std::size_t Pop() { // remove top
+	inline std::size_t Pop() { // remove top
 		--size;
 		return (size + 1);
 	}
