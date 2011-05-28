e2b8370b10b8f3ffec5c09f027912658359eeda0
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-05-28 10:27:37 +0000

    BF works oK?

--- 2009011419_hw4/scheduler/brute-force.cc
+++ 2009011419_hw4/scheduler/brute-force.cc
@@ -3,8 +3,19 @@
 #include <cstdio>
 #include <iostream>
 #include <stdint.h>
+#include <new>
+#include <cstddef>
 
 using namespace std;
+
+template <class T>
+struct Node {
+	Node *next;
+	T data;
+
+	inline Node() : next(NULL) {
+	}
+};
 
 class Scheduler {
 private:
@@ -62,27 +73,55 @@ public:
 	inline ~Scheduler();
 
 	inline void Proc() {
-		for (size_t i = 0; i != m; ++i) {
-			cout << 1 << endl;
+		Task tmp0;
+		for (size_t i = 0; (i != m) && sched->next; ++i) {
+			tmp0 = GetMin();
+			printf("%s\n", tmp0.ID);
+			if (!QuitSched(tmp0)) {
+				PutIn(tmp0);
+			}
 		}
 	}
 
 private:
 	size_t n, m;
 	char **task;
+	Node<Task> *sched;
 
-	inline bool QuitSched(Prior &pr) {
+	inline bool QuitSched(Task &p) {
 		// determine if pr should quit scheduling or continue, pr is modified
 		// true if should quit, else false
-		if (0 != (0x80000000 & pr)) {
+		if (0 != (0x80000000 & p.pr)) {
 			return true;
 		}
-		pr = pr << 1;
+		p.pr = p.pr << 1;
 		return false;
+	}
+
+	inline Task GetMin() {
+		Task tmp = sched->next->data;
+		Node<Task> *tmp0 = sched->next;
+		sched->next = sched->next->next;
+		delete tmp0;
+		return tmp;
+	}
+
+	inline void PutIn(Task const &t) {
+		Node<Task> *tmp = new (nothrow) Node<Task>;
+		Node<Task> *tmp1 = sched;
+		Node<Task> *tmp2 = sched->next;
+		while (tmp2 && (tmp2->data < t)) {
+			tmp1 = tmp2;
+			tmp2 = tmp2->next;
+		}
+		tmp1->next = tmp;
+		tmp->next = tmp2;
+		tmp->data = t;
 	}
 };
 
 inline Scheduler::Scheduler() {
+	sched = new (nothrow) Node<Task>;
 	cin >> n >> m;
 	task = new (nothrow) char *[n];
 	Task tmp0;
@@ -90,6 +129,7 @@ inline Scheduler::Scheduler() {
 		task[i] = new (nothrow) char[kMaxNameLen];
 		scanf("%u %s", &tmp0.pr, task[i]);
 		tmp0.ID = task[i];
+		PutIn(tmp0);
 	}
 }
 
@@ -98,6 +138,12 @@ inline Scheduler::~Scheduler() {
 		delete [] task[i];
 	}
 	delete [] task;
+	Node<Task> *tmp1 = sched;
+	while (tmp1) {
+		Node<Task> *tmp2 = tmp1;
+		tmp1 = tmp1->next;
+		delete tmp2;
+	}
 }
 
 int main() {
