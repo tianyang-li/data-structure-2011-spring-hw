17b2904bd427b2faeaefdbb4f83fdf62e57613c5
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-05-29 03:21:46 +0000

    add more functionalities to pr que

--- 2009011419_hw4/navigator/main.cc
+++ 2009011419_hw4/navigator/main.cc
@@ -21,20 +21,72 @@ struct Node {
 };
 
 class Navigator {
+	typedef int Dist;
+
+	struct Road {
+		Dist d;
+		int to;  // index of the city it goes to
+	};
+
 	typedef int Cost;
+
+	static Cost const kInfCost = 10000000;
 
 	struct City {
 		Cost cost;
 		int cap_rem;  // remaining capacity
 		int prev_price;
+		int gas_price;
+		Node<Road> *road;
+
+		inline City() {
+			road = new (nothrow) Node<Road>;
+		}
+
+		inline ~City() {
+			Node<Road> *tmp1 = road, *tmp2;
+			while (tmp1) {
+				tmp2 = tmp1;
+				tmp1 = tmp1->next;
+				delete tmp2;
+			}
+		}
 
 		inline bool operator<(City const &x) const {
 			return (cost < x.cost);
+		}
+
+		inline void InitCost() {
+			cost = kInfCost;
+		}
+
+		inline void AddRoad(int const to, Dist const d) {
+			Node<Road> *tmp = new (nothrow) Node<Road>;
+			tmp->data.d = d;
+			tmp->data.to = to;
+			tmp->next = road->next;
+			road->next = tmp;
 		}
 	};
 
 	struct CityHandle {
 		City *city;
+
+		inline CityHandle() {
+		}
+
+		inline ~CityHandle() {
+		}
+
+		inline CityHandle &operator=(CityHandle const &x) {
+			if (this != &x) {
+				city = x.city;
+			}
+			return *this;
+		}
+
+		inline CityHandle(CityHandle const &x) : city(x.city) {
+		}
 
 		inline bool operator<(CityHandle const &x) const {
 			return (*city < *x.city);
@@ -43,9 +95,23 @@ class Navigator {
 
 public:
 	inline Navigator() {
+		int m;
+		cin >> n >> m;
+		min_que.SetSize(n);
+		cities = new (nothrow) City[n];
+		for (int i = 0; i != n; ++i) {
+			scanf("%d", &cities[i].gas_price);
+		}
+		int c1, c2, d;
+		for (int i = 0; i != m; ++i) {
+			scanf("%d %d %d", &c1, &c2, &d);
+			cities[c1].AddRoad(c2, d);
+			cities[c2].AddRoad(c1, d);
+		}
 	}
 
 	inline ~Navigator() {
+		delete [] cities;
 	}
 
 	inline void Proc() {
@@ -54,15 +120,67 @@ public:
 		int c, s, t;
 		for (int i = 0; i != 1; ++i) {
 			scanf("%d %d %d", &c, &s, &t);
+			printf("%d\n", Query(c, s, t));
 		}
 	}
 
 private:
 	int n;  // # of cities
-	int m;  // # of roads
+	City *cities;
+	PrQue<CityHandle> min_que;
 
 	inline Cost Query(int const c, int const s, int const t) {
-		return -1;
+		for (int i = 0; i != n; ++i) {
+			cities[i].InitCost();
+		}
+		cities[s].cost = 0;
+		cities[s].cap_rem = 0;
+		cities[s].prev_price = kInfCost;
+		CityHandle ct_hdl;
+		for (int i = 0; i != n; ++i) {
+			ct_hdl.city = &cities[i];
+			min_que.Insert(ct_hdl);
+		}
+		while (0 != min_que.size) {
+			ct_hdl = min_que.data[0];
+			min_que.Pop();
+			Node<Road> *adj = ct_hdl.city->road->next;
+			Cost new_cost;
+			while (adj) {
+				if (c >= adj->data.d) {
+					new_cost = ct_hdl.city->cost;
+					if ((0 != ct_hdl.city->cap_rem) && (ct_hdl.city->gas_price > ct_hdl.city->prev_price)) {
+						if (ct_hdl.city->cap_rem >= adj->data.d) {
+							new_cost += (adj->data.d * ct_hdl.city->prev_price);
+							if (new_cost < cities[adj->data.to].cost) {
+								cities[adj->data.to].cap_rem = ct_hdl.city->cap_rem - adj->data.d;
+								cities[adj->data.to].prev_price = ct_hdl.city->prev_price;
+								cities[adj->data.to].cost = new_cost;
+							}
+						}
+						else {
+							new_cost += ((adj->data.d - ct_hdl.city->cap_rem) * ct_hdl.city->gas_price
+									+ ct_hdl.city->cap_rem * ct_hdl.city->prev_price);
+							if (new_cost < cities[adj->data.to].cost) {
+								cities[adj->data.to].cap_rem = c - (adj->data.d - ct_hdl.city->cap_rem);
+								cities[adj->data.to].prev_price = ct_hdl.city->gas_price;
+								cities[adj->data.to].cost = new_cost;
+							}
+						}
+					}
+					else {
+						new_cost += (adj->data.d * ct_hdl.city->gas_price);
+						if (new_cost < cities[adj->data.to].cost) {
+							cities[adj->data.to].prev_price = ct_hdl.city->gas_price;
+							cities[adj->data.to].cap_rem = c - adj->data.d;
+							cities[adj->data.to].cost = new_cost;
+						}
+					}
+				}
+				adj = adj->next;
+			}
+		}
+		return ((kInfCost == cities[t].cost) ? -1 : cities[t].cost);
 	}
 };
 
