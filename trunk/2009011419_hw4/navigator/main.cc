00946fe92a9b520a660e098d73e525cbdf09423e
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-05-29 01:28:43 +0000

    WTF!

--- trunk/2009011419_hw4/navigator/main.cc
+++ trunk/2009011419_hw4/navigator/main.cc
@@ -1,19 +1,55 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
+#include <cstddef>
+#include <new>
+
+#include "priority-queue.h"
 
 using namespace std;
 
+template <class T>
+struct Node {
+	Node *next;
+	T data;
+
+	inline Node() : next(NULL) {
+	}
+
+	inline Node(T const &new_data) : next(NULL), data(new_data) {
+	}
+};
+
 class Navigator {
+	typedef int Cost;
+
 public:
 	inline Navigator() {
 	}
+
 	inline ~Navigator() {
 	}
 
+	inline void Proc() {
+		int q;
+		cin >> q;
+		int c, s, t;
+		for (int i = 0; i != 1; ++i) {
+			scanf("%d %d %d", &c, &s, &t);
+		}
+	}
+
 private:
+	int n;  // # of cities
+	int m;  // # of roads
+
+	inline Cost Query(int const c, int const s, int const t) {
+		return -1;
+	}
 };
 
 int main() {
+	Navigator nav;
+	nav.Proc();
 	return 0;
 }
