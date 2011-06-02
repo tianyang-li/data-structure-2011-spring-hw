6e40adbd88396cc3045b3da18b299fb78c698b3c
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-06-02 23:31:59 +0000

    working on it, hopefully to get it fone this morning

--- trunk/2009011419_hw4/navigator/main.cc
+++ trunk/2009011419_hw4/navigator/main.cc
@@ -8,18 +8,60 @@
 
 using namespace std;
 
+template <class T>
+struct Node {
+	Node *next;
+	T data;
+};
+
 class Navigator {
+	struct Road {
+		int to;  // index of the city int leads to
+		int d;  // distance to that city
+	};
+	struct City {
+		Node<Road> *adj;
+
+		inline City() {
+			adj = new (nothrow) Node<City>;
+		}
+
+		inline ~City() {
+			Node<Road> *tmp1 = adj, *tmp2;
+			while (tmp1) {
+				tmp2 = tmp1;
+				tmp1 = tmp1->next;
+				delete tmp2;
+			}
+		}
+
+		inline void AddRoad(int const to, int const d) {
+			Node<Road> *tmp = new (nothrow) Road;
+			tmp->data.d = d;
+			tmp->data.to = to;
+		}
+	};
+
 public:
-	Navigator() {
+	inline Navigator() {
+		int n, m;
+		cin >> n >> m;
+		price = new (nothrow) int[n];
+		for (int i = 0; i != n; ++i) {
+			scanf("%d", &price[i]);
+		}
+		int c1, c2, d;
 	}
 
-	~Navigator() {
+	inline ~Navigator() {
+		delete [] price;
 	}
 
-	void Proc() {
+	inline void Proc() {
 	}
 
 private:
+	int *price;
 };
 
 int main() {
