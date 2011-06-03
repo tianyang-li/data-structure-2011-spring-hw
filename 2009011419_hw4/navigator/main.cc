cbb230f6c95f5f8869a5665537dca31d44a7d27d
tmy1018 <tmy1018@e2de78ca-8d68-e27f-d6ba-9721914d3ef8>
2011-06-03 02:45:45 +0000

    hopefully to get it done b4 lunch

--- 2009011419_hw4/navigator/main.cc
+++ 2009011419_hw4/navigator/main.cc
@@ -15,15 +15,41 @@ struct Node {
 };
 
 class Navigator {
+	static int const kInfCost = 100000000;
+	static int const kMaxFuel = 1000;
+
+	struct CostHandle;
+	friend class PrQue<CostHandle>;
+
+	struct Cost {
+		int cost;
+		int pq_index;  // index in priority queue
+
+		inline bool operator<(Cost const &x) const {
+			return (cost < x.cost);
+		}
+	};
+
+	struct CostHandle {
+		Cost *cost_ptr;
+		int city;
+		int cap;  // capacity
+
+		inline bool operator<(CostHandle const &x) const {
+			return (*cost_ptr < *x.cost_ptr);
+		}
+	};
+
 	struct Road {
 		int to;  // index of the city int leads to
 		int d;  // distance to that city
 	};
+
 	struct City {
 		Node<Road> *adj;
 
 		inline City() {
-			adj = new (nothrow) Node<City>;
+			adj = new (nothrow) Node<Road>;
 		}
 
 		inline ~City() {
@@ -36,33 +62,143 @@ class Navigator {
 		}
 
 		inline void AddRoad(int const to, int const d) {
-			Node<Road> *tmp = new (nothrow) Road;
+			Node<Road> *tmp = new (nothrow) Node<Road>;
 			tmp->data.d = d;
 			tmp->data.to = to;
+			tmp->next = adj->next;
+			adj->next = tmp;
 		}
 	};
 
 public:
 	inline Navigator() {
-		int n, m;
+		int m;
 		cin >> n >> m;
 		price = new (nothrow) int[n];
+		city = new (nothrow) City[n];
 		for (int i = 0; i != n; ++i) {
 			scanf("%d", &price[i]);
 		}
 		int c1, c2, d;
+		for (int i = 0; i != m; ++i) {
+			scanf("%d %d %d", &c1, &c2, &d);
+			city[c1].AddRoad(c2, d);
+			city[c2].AddRoad(c1, d);
+		}
+		cost = new (nothrow) Cost *[n];
+		for (int i = 0; i != n; ++i) {
+			cost[i] = new (nothrow) Cost[kMaxFuel];
+		}
+		min_que.SetSize(n * kMaxFuel);
 	}
 
 	inline ~Navigator() {
 		delete [] price;
+		delete [] city;
+		for (int i = 0; i != n; ++i) {
+			delete [] cost[i];
+		}
+		delete [] cost;
 	}
 
 	inline void Proc() {
+		int q;
+		cin >> q;
+		int c, s, t;
+		for (int i = 0; i != q; ++i) {
+			scanf("%d %d %d", &c, &s, &t);
+			printf("%d\n", Query(c, s, t));
+		}
 	}
 
 private:
+	int n;
 	int *price;
+	City *city;
+	Cost **cost;
+	PrQue<CostHandle> min_que;
+
+	inline int Query(int const c, int const s, int const t);
+
+	inline void InitCost() {
+		for (int i = 0; i != n; ++i) {
+			for (int j = 0; j != kMaxFuel; ++j) {
+				cost[i][j].cost = kInfCost;
+			}
+		}
+	}
+
+	inline void RelaxEdges(CostHandle const &ch, int const c);
+	inline void ReduceCost(int const city, int const cap);
 };
+
+template <>
+inline void PrQue<Navigator::CostHandle>::PQSwap(Navigator::CostHandle &x, Navigator::CostHandle &y) {
+	int tmp = x.cost_ptr->pq_index;
+	x.cost_ptr->pq_index = y.cost_ptr->pq_index;
+	y.cost_ptr->pq_index = tmp;
+	Navigator::Cost *tmp_cost = x.cost_ptr;
+	x.cost_ptr = y.cost_ptr;
+	y.cost_ptr = tmp_cost;
+	tmp = x.city;
+	x.city = y.city;
+	y.city = tmp;
+	tmp = x.cap;
+	x.cap = y.cap;
+	y.cap = tmp;
+}
+
+inline int Navigator::Query(int const c, int const s, int const t) {
+	InitCost();
+	cost[s][0].cost = 0;
+	int pq_ind = 0;
+	CostHandle ch;
+	for (int i = 0; i != n; ++i) {
+		for (int j = 0; j != c; ++j) {
+			ch.cost_ptr = &cost[i][j];
+			ch.cost_ptr->pq_index = pq_ind;
+			min_que.Insert(ch);
+			ch.cap = j;
+			ch.city = i;
+			pq_ind++;
+		}
+	}
+	while (0 != min_que.size) {
+		ch = min_que.data[0];
+		min_que.Pop();
+		if (ch.city == t) {
+			return ch.cost_ptr->cost;
+		}
+		RelaxEdges(ch, c);
+	}
+	return -1;
+}
+
+inline void Navigator::RelaxEdges(Navigator::CostHandle const &ch, int const c) {
+	int cur_cost;
+	if (ch.cap < c) {
+		cur_cost = cost[ch.city][ch.cap].cost + price[ch.city];
+		if (cur_cost < cost[ch.city][ch.cap + 1].cost) {
+			cost[ch.city][ch.cap + 1].cost = cur_cost;
+			ReduceCost(ch.city, ch.cap + 1);
+		}
+	}
+	Node<Road> *adj = city[ch.city].adj->next;
+	int rem;  // remaining fuel
+	while (adj) {
+		rem = ch.cap - adj->data.d;
+		if (rem >= 0) {
+			if (cost[ch.city][ch.cap].cost < cost[adj->data.to][rem].cost) {
+				cost[adj->data.to][rem].cost = cost[ch.city][ch.cap].cost;
+				ReduceCost(adj->data.to, rem);
+			}
+		}
+		adj = adj->next;
+	}
+}
+
+inline void Navigator::ReduceCost(int const city, int const cap) {
+}
 
 int main() {
 	Navigator nav;
